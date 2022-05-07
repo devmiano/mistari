58567278fa8ecb9714cc7ca550008ce1222d6bde
Sammy Muchiri <dev.miano@gmail.com>
2022-05-07 20:48:27 +0300

    added migration to the database

--- manage.py
+++ manage.py
@@ -1,12 +1,20 @@
 from app import create_app,db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Role
 
 app = create_app('development')
 
+
 manager = Manager(app)
 '''function to import and execute the application with the specified server'''
 manager.add_command('server', Server)
+
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
 @manager.command
 def test():
   """Run the unit tests."""
