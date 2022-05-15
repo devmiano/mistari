38b313c9e4a24811e81ad0e6a240fabefbafd8b3
Sammy Muchiri <dev.miano@gmail.com>
2022-05-15 20:34:30 +0300

    recreated the schema

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ export MAIL_PASSWORD=2103DD5EF304BB7205A2B4EE2C0B9CE19FC9BFEE
 python3 manage.py server
 # python3 manage.py shell
 # python3 manage.py db init
-# python3 manage.py db migrate -m 'schema update'
+# python3 manage.py db migrate -m 'new schema'
 # python3 manage.py db upgrade
 # python3 manage.py test
